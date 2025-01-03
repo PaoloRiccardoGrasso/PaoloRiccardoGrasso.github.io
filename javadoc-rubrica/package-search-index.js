78e208eea765b4b6e91260b272b4aa1e42a8bdc3
Paolo Riccardo Grasso <grassopaoloriccardo@gmail.com>
2025-01-03 22:39:46 +0100

    aggiornamento javadoc

---
+++ javadoc-rubrica/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"io.github.PaoloRiccardoGrasso"},{"l":"io.github.PaoloRiccardoGrasso.Controller"},{"l":"io.github.PaoloRiccardoGrasso.Model"},{"l":"io.github.PaoloRiccardoGrasso.View"}];updateSearchResults();
