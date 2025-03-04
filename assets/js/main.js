6996ddee874df94c2a59fa0f5cb37cf7ea6ad9b8
Paolo Riccardo Grasso <grassopaoloriccardo@gmail.com>
2025-03-04 21:23:35 +0100

    Aggiornamento Sito

--- assets/js/main.js
+++ assets/js/main.js
@@ -138,4 +138,33 @@ sr.reveal('.services__card, .projects__card', { interval: 100 });
 
 
 
+
+
+
+const cursor = document.querySelectorAll(".cursor");
+const links = document.querySelectorAll(".link");
+
+window.addEventListener("mousemove", (e) => {
+  
+  let x = e.pageX;
+  let y = e.pageY;
+  
+  cursor.forEach(el => {
+    el.style.left = `${x}px`;
+    el.style.top = `${y}px`;
     
+    links.forEach(link => {
+      link.addEventListener("mouseenter", () => {
+        el.classList.add("hover");
+      })
+      link.addEventListener("mouseleave", () => {
+        el.classList.remove("hover");
+      })
+    })
+    
+  })
+  
+})
+
+
+
