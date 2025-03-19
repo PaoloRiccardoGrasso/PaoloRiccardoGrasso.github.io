a8d49ec8ce563bad3135c106f4ceb951e7a02202
Paolo Riccardo Grasso <grassopaoloriccardo@gmail.com>
2025-03-19 11:40:08 +0100

    Aggiornamento

--- SitoIronMan/JAVASCRIPT/script.js
+++ SitoIronMan/JAVASCRIPT/script.js
@@ -27,3 +27,42 @@ new Swiper('.card-wrapper', {
     }
 
   });
+
+
+
+
+
+  let next = document.querySelector('.next')
+let prev = document.querySelector('.prev')
+
+next.addEventListener('click', function(){
+    let items = document.querySelectorAll('.item')
+    document.querySelector('.slide').appendChild(items[0])
+})
+
+prev.addEventListener('click', function(){
+    let items = document.querySelectorAll('.item')
+    document.querySelector('.slide').prepend(items[items.length - 1]) // here the length of items = 6
+})
+
+
+
+document.querySelectorAll('.item').forEach(item => {
+    const video = item.querySelector('video');
+    const cover = item.querySelector('img.cover');
+
+    // Quando entri nell'item
+    item.addEventListener('mouseenter', () => {
+        cover.style.opacity = "0";
+        video.style.opacity = "1";
+        video.play();
+    });
+
+    // Quando esci dall'item
+    item.addEventListener('mouseleave', () => {
+        cover.style.opacity = "1";
+        video.style.opacity = "0";
+        video.pause();
+        video.currentTime = 0; // Riavvia il video
+    });
+});
